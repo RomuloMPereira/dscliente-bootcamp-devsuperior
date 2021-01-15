c74ee9393381521538534a5165bad24e357f3c2b
Romulo Pereira <mp.romulo@gmail.com>
2021-01-15 17:21:47 -0300

    Database seeding

--- src/main/java/com/devsuperior/dscliente/resources/ClientResource.java
+++ src/main/java/com/devsuperior/dscliente/resources/ClientResource.java
@@ -1,7 +1,5 @@
 package com.devsuperior.dscliente.resources;
 
-import java.time.Instant;
-import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
---
+++ src/main/resources/data.sql
@@ -0,0 +1,10 @@
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Maria', '65432692007', 2000.0, TIMESTAMP WITH TIME ZONE '1988-07-14T10:00:00Z', 2);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('João', '64037946041', 1000.0, TIMESTAMP WITH TIME ZONE '2002-08-17T10:00:00Z', 1);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Pedro', '30428353088', 2500.0, TIMESTAMP WITH TIME ZONE '1990-02-25T10:00:00Z', 3);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Renata', '72557562041', 1200.0, TIMESTAMP WITH TIME ZONE '1998-10-22T10:00:00Z', 2);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Carlos', '16364310036', 800.0, TIMESTAMP WITH TIME ZONE '1995-05-06T10:00:00Z', 0);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Bruno', '70314057005', 900.0, TIMESTAMP WITH TIME ZONE '1997-11-14T10:00:00Z', 1);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Francisco', '09741651074', 3000.0, TIMESTAMP WITH TIME ZONE '1984-04-27T10:00:00Z', 4);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Antonio', '68545555075', 5000.0, TIMESTAMP WITH TIME ZONE '1963-06-19T10:00:00Z', 1);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Ana', '89801034092', 6000.0, TIMESTAMP WITH TIME ZONE '1975-07-19T10:00:00Z', 2);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Rafael', '01047666030', 10200.0, TIMESTAMP WITH TIME ZONE '1978-09-20T10:00:00Z', 0);
