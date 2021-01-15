53514e1f1706de404654f5a6795753fb380369d3
Romulo Pereira <mp.romulo@gmail.com>
2021-01-15 16:45:25 -0300

    Data persistence with H2 database and JPA

--- src/main/java/com/devsuperior/dscliente/resources/ClientResource.java
+++ src/main/java/com/devsuperior/dscliente/resources/ClientResource.java
@@ -4,22 +4,25 @@ import java.time.Instant;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.devsuperior.dscliente.entities.Client;
+import com.devsuperior.dscliente.services.ClientService;
 
 @RestController
 @RequestMapping(value = "/clients")
 public class ClientResource {
 	
+	@Autowired
+	private ClientService service;
+	
 	@GetMapping
 	public ResponseEntity<List<Client>> findAll(){
-		List<Client> list = new ArrayList<>();
-		list.add(new Client(1L, "João", "52177801001", 1000.0, Instant.now(), 2));
-		list.add(new Client(2L, "Maria", "65432692007", 2000.0, Instant.now(), 1));
+		List<Client> list = service.findAll();
 		return ResponseEntity.ok().body(list);				
 	}
 
